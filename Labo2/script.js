ecc5176b2065b1ab4c8409dc256fa7d0bf64ad45
Matthieu BRAULT <matthieu.brault@epitech.eu>
2019-09-13 13:51:23 -0400

    propreté

---
+++ Labo2/script.js
@@ -0,0 +1,24 @@
+const text = document.getElementById("text");
+const button = document.getElementById("button");
+const input = document.getElementById("input");
+const datalistOptions = [...document.getElementById("datalist").options];
+const values = datalistOptions.map(({ value }) => value);
+
+input.oninput = () => {
+	const filteredOptions = values.filter(el => (el.indexOf(input.value) !== -1))
+	if (filteredOptions.length === 0) {
+		text.hidden = false
+	} else {
+		text.hidden = true
+	}
+	if (input.value === "") {
+		button.hidden = true;
+	} else {
+		button.hidden = false;
+	}
+}
+
+button.onclick = () => {
+	text.hidden = true;
+	button.hidden = true;
+}
