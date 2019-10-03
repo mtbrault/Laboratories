c8373d3014351e656c681ba493f7e78a31834fdd
valerian huylebroeck <valerian.huylebroeck@epitech.eu>
2019-10-02 21:02:41 -0400

    fix delete task

--- Labo4/script.js
+++ Labo4/script.js
@@ -1,89 +1,94 @@
 window.onload = function() {
-	var listToDo = document.getElementById("todo");
-	const Manager = new ApiManager(this.localStorage.getItem("userId"), listToDo, 'https://glo3102lab4.herokuapp.com');
-	const addButton = document.getElementById("buttonAdd");
-	const input = document.getElementById("taskName");
-	const refresh = document.getElementById("refreshToken");
+    var listToDo = document.getElementById("todo");
+    const Manager = new ApiManager(this.localStorage.getItem("userId"), listToDo, 'https://glo3102lab4.herokuapp.com');
+    const addButton = document.getElementById("buttonAdd");
+    const input = document.getElementById("taskName");
+    const refresh = document.getElementById("refreshToken");
 
-	addButton.addEventListener('click', () => {
-		Manager.addTask(input.value);
-	});
-	refresh.addEventListener('click', () => {
-		Manager.initToken();
-	})
+    addButton.addEventListener('click', () => {
+        Manager.addTask(input.value);
+    });
+    refresh.addEventListener('click', () => {
+        Manager.initToken();
+    })
 }
 
 class ApiManager {
 
-	constructor(token, table, url) {
-		this.table = table;
-		this.url = url;
-		if (token === null)
-			this.initToken();
-		else {
-			this.userId = token;
-			this.loadTask();
-		}
-	}
+    constructor(token, table, url) {
+        this.table = table;
+        this.url = url;
+        if (token === null)
+            this.initToken();
+        else {
+            this.userId = token;
+            this.loadTask();
+        }
+    }
 
-	async initToken() {
-		const response = await axios.post(this.url + '/users');
-		this.userId = response.data.id;
-		localStorage.setItem("userId", this.userId);
-		this.loadTask();
-	}
+    initToken = async() => {
+        try {
+            const response = await axios.post(this.url + '/users');
+            this.userId = response.data.id;
+            localStorage.setItem("userId", this.userId);
+        } catch (e) {
+            alert("Cannot init token");
+        }
+        this.loadTask();
+    }
 
-	async addTask(value) {
-		if (value === "") {
-			alert("Veuillez nommer votre tâche");
-			return ;
-		}
-		await axios.post(this.url + '/' + this.userId + '/tasks', {
-			name: value,
-		}, {
-			  headers: {
-				'Content-Type': 'application/json',
-			  }
-		});
-		this.loadTask();
-	}
+    addTask = async(value) => {
+        if (value === "") {
+            alert("Please task need at least 1 character");
+            return;
+        }
+        try {
+            await axios.post(this.url + '/' + this.userId + '/tasks', {
+                name: value,
+            }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+        } catch (e) {
+            alert("Cannot add task");
+        }
+        this.loadTask();
+    }
 
-	async loadTask() {
-		this.table.innerHTML = "";
-		await axios.get(this.url + '/' + this.userId + '/tasks')
-		.then(response => {
-			let size = 0;
-			response.data.tasks.forEach((value, index) => {
-				this.table.innerHTML += "<tr><td>" + value.name + "</td><td class=\"update\"><button type=\"button\" class=\"btn btn-info\">Modifier</td><td class=\"delete\"><button id=\"" + index + "\" type=\"button\" class=\"btn btn-danger\">Supprimer</button></td></tr>";
-				size++;
-			});
-			for (var i = 0; i < size; i++) {
-				(function(j, func) {
-					document.getElementById(j).addEventListener('click', function() {
-						func(j);
-					});
-				}(i, this.deleteTask))
-			}
-		})
-		.catch(err => {
-			this.initToken();
-		});
-	}
+    deleteTask = async(id) => {
+        try {
+            await axios.delete(this.url + '/' + this.userId + '/tasks/' + id);
+        } catch (e) {
+            alert("Cannot delete this task")
+        }
+        this.loadTask();
+    }
 
-	async deleteTask(id) {
-		await axios.delete(this.url + '/' + this.userId + '/tasks/' + id);
-		this.loadTask();
-	}
+    updateTask = async(id, value) => {
+        await axios.put(this.url + '/' + this.userId + '/tasks/' + id, {
+            name: value,
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        this.loadTask();
+    }
 
-	async updateTask(id, value) {
-		await axios.put(this.url + '/' + this.userId + '/tasks/' + id, {
-			name: value,
-		}, {
-			  headers: {
-				'Content-Type': 'application/json',
-			  }
-		});
-		this.loadTask();
-	}
-
+    loadTask = async()  => {
+        try {
+            this.table.innerHTML = "";
+            const { data: { tasks } } = await axios.get(this.url + '/' + this.userId + '/tasks')
+            for (var x = 0; x < tasks.length; x++) {
+                this.table.innerHTML += "<tr><td>" + tasks[x].name + "</td><td class=\"update\"><button type=\"button\" class=\"btn btn-info\">Modifier</td><td class=\"delete\"><button id=\"" + x + "\" type=\"button\" class=\"btn btn-danger\">Supprimer</button></td></tr>";
+            };
+            for (var i = 0; i < tasks.length; i++) {
+                const id = tasks[i].id
+                document.getElementById(i).addEventListener('click', () => this.deleteTask(id));
+            }
+        } catch (e) {
+            alert("Cannot get Task")
+        }
+    }
 }
